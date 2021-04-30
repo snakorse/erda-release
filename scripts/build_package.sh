9cbbe8cb33880188b0559e6646412f186f5700cf
Muzry <muzrry@gmail.com>
2021-04-30 17:45:30 +0800

    mkdir pacakge

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -2,6 +2,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 rm -rf ./erda-actions
 
+rm -rf ../package
+
+mkdir ../package
+
 git clone https://github.com/erda-project/erda-actions.git
 
 tar -cvzf ../package/erda-release.tar.gz ../../erda-release
