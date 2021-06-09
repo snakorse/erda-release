000f9a1cefb3d5e0624792e9587c1c5aae7e99c4
Muzry <muzrry@gmail.com>
2021-06-09 19:16:21 +0800

    update image repo url

--- build_package.sh
+++ build_package.sh
@@ -4,11 +4,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 rm -rf package
 mkdir package
 
-rm -rf ./erda/scripts/erda-actions
-git clone https://github.com/erda-project/erda-actions.git ./erda/scripts/
+rm -rf erda/scripts/erda-actions
+git clone https://github.com/erda-project/erda-actions.git erda/scripts/erda-actions
+rm -rf erda/scripts/erda-actions/.git
 
-rm -rf ./erda/scripts/erda-addons
-git clone https://github.com/erda-project/erda-addons.git ./erda/scripts/
+rm -rf erda/scripts/erda-addons
+git clone https://github.com/erda-project/erda-addons.git erda/scripts/erda-addons
+rm -rf erda/scripts/erda-addons/.git
 
 if [[ -z $ERDA_VERSION ]]; then
     echo "ERDA_VERSION is empty"
@@ -20,6 +22,15 @@ if [[ -z $ERDA_OS_SYSTEM ]]; then
     exit
 fi
 
-wget https://raw.githubusercontent.com/erda-project/erda/release/${ERDA_VERSION}/docs/guides/deploy/How-to-install-the-Erda.md -O ./erda/How-to-install-the-Erda.md
+rm -rf erda/How-to-install-Erda.md
+rm -rf erda/How-to-install-Erda-zh.md
+wget https://raw.githubusercontent.com/erda-project/erda/release/${ERDA_VERSION}/docs/guides/deploy/How-to-install-Erda.md -O erda/How-to-install-Erda.md 
+wget https://raw.githubusercontent.com/erda-project/erda/release/${ERDA_VERSION}/docs/guides/deploy/How-to-install-Erda-zh.md -O erda/How-to-install-Erda-zh.md
 
-tar -cvzf package/erda-${ERDA_OS_SYSTEM}.tar.gz erda/
+if [[ $ERDA_OS_SYSTEM == "linux" ]]; then
+    tar -cvzf package/erda-${ERDA_OS_SYSTEM}.tar.gz erda/
+fi
+
+if [[ $ERDA_OS_SYSTEM == "windows" ]]; then
+    zip -r package/erda-${ERDA_OS_SYSTEM}.zip erda/
+fi
